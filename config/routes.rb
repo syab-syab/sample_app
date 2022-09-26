ef045cf346ebb45cad46dc32367c9a1fb1cb7bc9
syab-syab <syabsyabsyabsyab@gmail.com>
2022-09-26 22:01:38 +0900

    3章まで終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  # URL名 = '[コントローラー名]/[アクション名]'
+  # httpメソッド 'URL'という感じ
+  # rootでは'[コントローラー名]#[アクション名]'で指定可能
+  root 'static_pages#home'
+  get  'static_pages/home'
+  get  'static_pages/help'
+  get  'static_pages/about'
 
-  get 'static_pages/help'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
