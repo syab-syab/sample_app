967976323ff4a5d0229b5712fb4107bffb376eee
syab-syab <syabsyabsyabsyab@gmail.com>
2022-09-28 01:15:18 +0900

    5章まで終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   # URL名 = '[コントローラー名]/[アクション名]'
   # httpメソッド 'URL'という感じ
   # rootでは'[コントローラー名]#[アクション名]'で指定可能
   root 'static_pages#home'
   # [url], to: [コントローラー名]#[アクション名]とすれば
   # 指定したurlにアクセスしたら指定したコントローラーのアクションを呼び出す
-  get  '/help', to: 'static_pages#help'
-  get  '/about', to: 'static_pages#about'
-  get  '/contact', to: 'static_pages#contact'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
 end
