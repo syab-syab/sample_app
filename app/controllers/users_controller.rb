630b92b4dacd07cd0c74188c3cd9309706c3ba85
syab-syab <syabsyabsyabsyab@gmail.com>
2022-09-28 23:21:37 +0900

    7章7.3まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  # /users/[idの数値]でアクセスできる
+  def show
+    # paramsはユーザーIDの読み出し
+    # params[:id]の部分はidの数値に置き換わる
+    @user = User.find(params[:id])
+  end
+
+  # ユーザー登録(Signup)
   def new
+    @user = User.new
   end
 end
