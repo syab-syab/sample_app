ed846a636620c833ee9947498c647d46fb9bcede
syab-syab <syabsyabsyabsyab@gmail.com>
2022-10-01 17:45:29 +0900

    8章8.2.5まで

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,13 @@ class User < ApplicationRecord
   has_secure_password
   # パスワードの存在性と最小文字数
   validates :password, presence: true, length: { minimum: 6 }
+
+  # 渡された文字列のハッシュ値を返す
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
 
 
